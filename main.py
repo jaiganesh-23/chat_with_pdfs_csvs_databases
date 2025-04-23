10ea5fa894316dcdd931f03f93baf73100d88582
jaiganesh-23 <jaiganeshmothilal22@gmail.com>
2025-04-23 15:50:11 +0530

    modified main host

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ if __name__ == "__main__":
     os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
     os.environ["MISTRAL_API_KEY"] = os.getenv("MISTRALAI_API_KEY")
 
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
