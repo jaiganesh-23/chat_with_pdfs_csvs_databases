cceef3050f67edc83da951a699ed2538a4224926
jaiganesh-23 <jaiganeshmothilal22@gmail.com>
2025-04-27 22:00:24 +0530

    changed rag config

--- application/views.py
+++ application/views.py
@@ -186,8 +186,8 @@ def tool"""+f"""{index}"""+f"""(query: str) -> str:
         if file["fileType"] == "pdf":
             preparedb_instance = PrepareVectorDB(
                 doc_dir=here(f"downloads/pdfs/{file['fileName']}"),
-                chunk_size=500,
-                chunk_overlap=100,
+                chunk_size=50,
+                chunk_overlap=10,
                 embedding_model="mistral-embed",
                 vectordb_dir=here(f"vectordbs/{file['fileName']}"),
                 collection_name=f"{file['fileName']}-chroma",
@@ -207,11 +207,11 @@ def tool"""+f"""{index}"""+f"""(query: str) -> str:
     rag_tool = InitRAGTool(
         embedding_model="mistral-embed",
         vectordb_dir=here(f"vectordbs/{file['fileName']}"),
-        k=2,
+        k=20,
         collection_name=f"{file['fileName']}-chroma"
     )
-    docs = rag_tool.vectordb.similarity_search(query, k=2)
-    return "".join([doc.page_content for doc in docs])
+    docs = rag_tool.vectordb.similarity_search(query, k=20)
+    return "\\n\\n".join([doc.page_content for doc in docs])
 """
             code = compile(func_string, "<string>","exec")
             code_namespace = {"__name__": "__main__"}
